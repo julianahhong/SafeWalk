287e0786d748f73fdbdd75b51be2396023019a82
Juliana Hong <julianahhong@gmail.com>
2015-09-05 11:55:20 -0400

    Update

--- SafeWalk/SafeWalk/ViewController.swift
+++ SafeWalk/SafeWalk/ViewController.swift
@@ -46,8 +46,45 @@ class ViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDel
             var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
             self.view = mapView
             
+            //marker
+            var marker = GMSMarker()
+            marker.position = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+//            marker.title = "Sydney"
+//            marker.snippet = "Australia"
+            marker.map = mapView
+
+            
+            
+            
+            
+            
             // 7
             locationManager.stopUpdatingLocation()
         }
     }
+    
+    
+//    @IBAction func pickPlace(sender: UIBarButtonItem) {
+//        let center = CLLocationCoordinate2DMake(51.5108396, -0.0922251)
+//        let northEast = CLLocationCoordinate2DMake(center.latitude + 0.001, center.longitude + 0.001)
+//        let southWest = CLLocationCoordinate2DMake(center.latitude - 0.001, center.longitude - 0.001)
+//        let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
+//        let config = GMSPlacePickerConfig(viewport: viewport)
+//        var placePicker = GMSPlacePicker(config: config)
+//        
+//        placePicker?.pickPlaceWithCallback({ (place: GMSPlace?, error: NSError?) -> Void in
+//            if let error = error {
+//                println("Pick Place error: \(error.localizedDescription)")
+//                return
+//            }
+//            
+//            if let place = place {
+//                println("Place name \(place.name)")
+//                println("Place address \(place.formattedAddress)")
+//                println("Place attributions \(place.attributions)")
+//            } else {
+//                println("No place selected")
+//            }
+//        })
+//    }
 }
