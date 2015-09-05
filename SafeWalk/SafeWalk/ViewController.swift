980562fd9fa819e8619e93f2b2329d145bfa9935
Juliana Hong <julianahhong@gmail.com>
2015-09-05 05:09:37 -0400

    Added GoogleMaps cocoapod

--- SafeWalk/SafeWalk/ViewController.swift
+++ SafeWalk/SafeWalk/ViewController.swift
@@ -7,19 +7,28 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var camera = GMSCameraPosition.cameraWithLatitude(-33.868,
+            longitude:151.2086, zoom:6)
+        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
+        
+        var marker = GMSMarker()
+        marker.position = camera.target
+        marker.snippet = "Hello World"
+        marker.appearAnimation = kGMSMarkerAnimationPop
+        marker.map = mapView
+        
+        self.view = mapView
+    }
 
 }
-
