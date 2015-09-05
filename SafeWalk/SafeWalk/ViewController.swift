c3878cf5415e4ef2a31bee3447cc323f65405ba9
Juliana Hong <julianahhong@gmail.com>
2015-09-05 04:11:35 -0400

    Created xcode project

---
+++ SafeWalk/SafeWalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SafeWalk
+//
+//  Created by Juliana Hong on 9/5/15.
+//  Copyright (c) 2015 Juliana Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
